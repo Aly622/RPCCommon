9a07eb639926e57bd7c85888d603c18893f4f679
Oliver.Liu <li_jian0812@126.com>
2016-04-01 15:38:09 +0800

    init

---
+++ src/main/java/com/RpcEncoder.java
@@ -0,0 +1,25 @@
+package com;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+/**
+ * Created by Oliver.Liu on 4/1/2016.
+ */
+public class RpcEncoder extends MessageToByteEncoder {
+
+    private Class<?> genericClass;
+    public RpcEncoder(Class<?> genericClass) {
+        this.genericClass = genericClass;
+    }
+
+    @Override
+    public void encode(ChannelHandlerContext ctx, Object in, ByteBuf out) throws Exception {
+        if (genericClass.isInstance(in)) {
+            byte[] data = SerializationUtil.serialize(in);
+            out.writeInt(data.length);
+            out.writeBytes(data);
+        }
+    }
+}
